2299274674a4759450a74e8fca0efb95061d0950
kimeswartz <kim.e.swartz@gmail.com>
2024-09-17 13:55:31 +0200

    added comment in controller class to explain the PUT method

--- src/main/java/com/useo/webserviceweatherapi/controller/WeatherController.java
+++ src/main/java/com/useo/webserviceweatherapi/controller/WeatherController.java
@@ -2,7 +2,7 @@ package com.useo.webserviceweatherapi.controller;
 
 import com.useo.webserviceweatherapi.model.WeatherInformation;
 import com.useo.webserviceweatherapi.service.WeatherService;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,6 +33,18 @@ public class WeatherController {
         weatherService.deleteWeatherById(id);
         return ResponseEntity.noContent().build();
     }
+
+    /**
+     * Handles HTTP PUT requests to update a weather record.
+
+     * - @PutMapping("/{id}"): Maps the PUT request to /api/weather/{id}, where {id} is a variable part of the URI.
+     * - @PathVariable Long id: Extracts the dynamic 'id' from the URL. This 'id' represents the identifier of the weather record to be updated.
+     * - @RequestBody WeatherInformation updatedInfo: Maps the incoming JSON request body to a WeatherInformation object,
+     *   which contains the updated weather data that will replace the existing record.
+
+     * The method interacts with the WeatherService to perform the update operation. If successful, it returns the updated
+     * WeatherInformation object along with an HTTP 200 OK response.
+     */
 
     @PutMapping("/{id}")
     public ResponseEntity<WeatherInformation> updateWeather(@PathVariable Long id, @RequestBody WeatherInformation updatedInfo) {

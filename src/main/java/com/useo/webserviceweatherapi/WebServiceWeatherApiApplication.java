cdb0caab0e26e1b03b4f035a6fd5db4192f8a2c4
kimeswartz <kim.e.swartz@gmail.com>
2024-09-17 09:50:30 +0200

    Initial commit to weather api web service

---
+++ src/main/java/com/useo/webserviceweatherapi/WebServiceWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.useo.webserviceweatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceWeatherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceWeatherApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/useo/webserviceweatherapi/WebServiceWeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.useo.webserviceweatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceWeatherApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
